20887ce0c9757517d3b9e666a1a3113906d3397e
YasaaMoin <yasaamoin97@gmail.com>
2017-08-09 04:47:57 +0530

    First Commit

---
+++ public/javascripts/chat.js
@@ -0,0 +1,63 @@
+   
+   //Relaying messages 
+   var Chat= function(socket) {
+   	this.socket = socket;
+   };
+
+
+   Chat.prototype.sendMessage = function(room,text) {
+
+   	var message ={
+   		room: room,
+   		text: text,
+   	};
+
+   	this.socket.emit('message',message);
+   };
+
+
+
+   //Room changes
+   Chat.prototype.changeRoom = function(room) {
+
+   	this.socket.emit('join', {
+
+   		newRoom: room
+   	});
+   };
+
+
+
+
+   //Chat commands:
+   //Joining Room,Changing nickname.
+   Chat.prototype.processCommand = function(command) {
+    var words = command.split(' ');
+    var command = words[0].substring(1,words[0].length).toLowerCase();
+
+    var message = false;
+
+    switch(command) {
+
+    	case  'join' :  words.shift();
+    	                var room = words.join( '  ' );
+    	                this.changeRoom(room);
+    	                break;
+
+    	case   'nick' :  words.shift();
+    	                 var name = words.join( ' ' );
+    	                 this.socket.emit('nameAttempt', name);
+    	                 break;
+
+    	       default:  message = 'Unrecognized command';
+    	                 break;
+
+    }
+    return message;
+
+   };
+
+
+
+
+   
